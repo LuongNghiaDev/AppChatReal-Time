afb85a23d6f378415585742be49a6cb6cbd71196
unknown <nguyenluongnghia412@gmail.com>
2022-07-08 11:32:23 +0700

    first commit

---
+++ DuAn/app/src/main/java/com/example/duan/listerners/ConversionListener.java
@@ -0,0 +1,8 @@
+package com.example.duan.listerners;
+
+import com.example.duan.models.User;
+
+public interface ConversionListener {
+
+    void onConversionClicked(User user);
+}
---
+++ DuAn/app/src/main/java/com/example/duan/listerners/UserListener.java
@@ -0,0 +1,8 @@
+package com.example.duan.listerners;
+
+import com.example.duan.models.User;
+
+public interface UserListener {
+
+    void onUserClicked(User user);
+}
